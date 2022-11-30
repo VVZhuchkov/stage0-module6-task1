d604e0bbfa9df61a10a4f1c1c842ce818b0ebe64
VVZhuchkov <vzhuchkov@tut.by>
2022-11-30 14:43:02 +0300

    wow

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,35 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal (String color, int numberOfPaws, boolean hasFur){
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription(){
+        String quantityPaws = "paw";
+        if (numberOfPaws>1) {
+            quantityPaws = "paws";
+        }
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + " " + quantityPaws + " and " + (hasFur? "a":"no") + " fur.";
+    }
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        System.out.println(dog.getDescription());
+        System.out.println();
+        Animal animal = new Dog();
+        System.out.println(animal.getDescription());
+        System.out.println();
+        Bird bird = new Bird();
+        System.out.println(bird.getDescription());
+        System.out.println();
+        Animal animal2 = new Bird();
+        System.out.println(animal2.getDescription());
+    }
 }
